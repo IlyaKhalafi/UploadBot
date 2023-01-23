23207daba5dc723046ce4830efdc9582e90be1e6
Ilya Khalafi <ilyakhalafi@yahoo.com>
2023-01-24 01:19:35 +0330

    Updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # UploadBot
 Simple telegram bot to upload and download files on telegram messenger
+
+## Usage
+This bot is made to be simple and has only 3 commands:
+
+- Shows bot welcome message through `/start` command
+- Send any document, audio, video, photo to bot and it will upload it to `transfer.sh` and sends you the download link
+- Send it a link to a file on the internet and it will download and send it to you on telegram
+
+## Installation
+
+Use the bash commands below to install and run the bot:
+
+````bash
+git clone https://github.com/IlyaKhalafi/UploadBot
+pip3 install -r requirements.txt
+# <Create config.py appropriately>
+python3 main.py
+````
+
+### Requirements
+
+* Python 3.6 or higher
+* Pyrogram
+* TgCrypto (Optional for Speedup)
+* uvloop (Optional for Speedup)
+
+### Variables
+
+* `APP_ID` Your API ID from my.telegram.org
+* `API_HASH` Your API Hash from my.telegram.org
+* `BOT_TOKEN` Your bot token from @BotFather
+* `MAX_FILE_SIZE` Maximum file size to upload in bytes, default is 200 MB
+* `REPL_URL` If you are running this bot on repl.it, you need to set this to your repl.it URL so bot will ping itself to keep it alive. Also using **uptimerobot.com** is highly recommended.
--- plugins/upload.py
+++ plugins/upload.py
@@ -3,6 +3,7 @@ import requests
 from pyrogram import Client, filters
 from config import MAX_FILE_SIZE
 
+
 def upload_to_transfersh(file_path):
 
     url = 'https://transfer.sh/'
@@ -11,46 +12,47 @@ def upload_to_transfersh(file_path):
     download_link = response.content.decode('utf-8')
     return download_link
 
+
 @Client.on_message(filters.private
-                & (filters.document | filters.photo | filters.audio | filters.video))
+                   & (filters.document | filters.photo | filters.audio | filters.video))
 async def upload(client, message):
-    
+
     # Checking if file size is greater than the defined limit
     if (message.document and message.document.file_size > MAX_FILE_SIZE):
         await message.reply_text(
             f'File is too big.\n\nFile size: {message.document.file_size}'
-            )
+        )
         return
     if (message.photo and message.photo.file_size > MAX_FILE_SIZE):
         await message.reply_text(
             f'Photo is too big.\n\nFile size: {message.photo.file_size}'
-            )
+        )
         return
     if (message.audio and message.audio.file_size > MAX_FILE_SIZE):
         await message.reply_text(
             f'Audio is too big.\n\nFile size: {message.audio.file_size}'
-            )
+        )
         return
     if (message.video and message.video.file_size > MAX_FILE_SIZE):
         await message.reply_text(
             f'Video is too big.\n\nFile size: {message.video.file_size}'
-            )
+        )
         return
 
     # Downloading the file
     await message.reply_text('Downloading file...', quote=True)
     path = await message.download()
     if not path:
-        await message.reply_text('Failed to download file. Please try again later...', quote=True)
+        await message.reply_text('Failed to download the file. Please try again later...', quote=True)
         return
 
     # Uploading the file to transfer.sh
-    await message.reply_text('Uploading file to transfer.sh...', quote=True)
+    await message.reply_text('Uploading the file to transfer.sh...', quote=True)
     link = upload_to_transfersh(path).removesuffix('\n')
-    
+
     # remove file in from local storage
     os.remove(path)
-    
+
     # Sending download link to user
     parts = link.split('/')
     parts.insert(3, 'get')

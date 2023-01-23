04e288c7156b4efdc50f67d03d2a1b88cadef3f5
Ilya Khalafi <ilyakhalafi@yahoo.com>
2023-01-23 10:35:10 +0330

    Added download functionality
    Implemented downloading file from direct links

--- plugins/download.py
+++ plugins/download.py
@@ -1,5 +1,21 @@
 from pyrogram import Client, filters
 
+photo_extensions = ['jpg', 'jpeg', 'png', 'gif', 'webp', 'bmp', 'tiff', 'svg']
+
+audio_extensions = ['mp3', 'wav', 'ogg', 'flac', 'aac', 'm4a', 'wma', 'alac', 'opus']
+
+video_extensions = ['mp4', 'mkv', 'avi', 'webm', 'flv', 'wmv', 'mov', 'mpeg', '3gp']
+
 @Client.on_message(filters.text & filters.private)
-async def download(client, message):
-    pass
+async def downloadage(client, message):
+    try:
+        if message.text.endswith(tuple(photo_extensions)):
+            await message.reply_photo(photo=message.text, quote=True)
+        elif message.text.endswith(tuple(audio_extensions)):
+            await message.reply_audio(audio=message.text, quote=True)
+        elif message.text.endswith(tuple(video_extensions)):
+            await message.reply_video(video=message.text, quote=True)
+        else:
+            await message.reply_document(document=message.text, quote=True)
+    except:
+        await message.reply_text("Something went wrong. Probably the download link is invalid?", quote=True)
--- plugins/upload.py
+++ plugins/upload.py
@@ -3,7 +3,6 @@ import requests
 from pyrogram import Client, filters
 from config import MAX_FILE_SIZE
 
-
 def upload_to_transfersh(file_path):
 
     url = 'https://transfer.sh/'
@@ -15,43 +14,47 @@ def upload_to_transfersh(file_path):
 @Client.on_message(filters.private
                 & (filters.document | filters.photo | filters.audio | filters.video))
 async def upload(client, message):
+    
+    # Checking if file size is greater than the defined limit
     if (message.document and message.document.file_size > MAX_FILE_SIZE):
         await message.reply_text(
             f'File is too big.\n\nFile size: {message.document.file_size}'
             )
         return
-
     if (message.photo and message.photo.file_size > MAX_FILE_SIZE):
         await message.reply_text(
             f'Photo is too big.\n\nFile size: {message.photo.file_size}'
             )
         return
-
     if (message.audio and message.audio.file_size > MAX_FILE_SIZE):
         await message.reply_text(
             f'Audio is too big.\n\nFile size: {message.audio.file_size}'
             )
         return
-
     if (message.video and message.video.file_size > MAX_FILE_SIZE):
         await message.reply_text(
             f'Video is too big.\n\nFile size: {message.video.file_size}'
             )
         return
 
-    await message.reply_text('Downloading file...')
+    # Downloading the file
+    await message.reply_text('Downloading file...', quote=True)
     path = await message.download()
     if not path:
-        await message.reply_text('Failed to download file. Please try again later...')
+        await message.reply_text('Failed to download file. Please try again later...', quote=True)
         return
 
-    await message.reply_text('Uploading file to transfer.sh...')
+    # Uploading the file to transfer.sh
+    await message.reply_text('Uploading file to transfer.sh...', quote=True)
     link = upload_to_transfersh(path).removesuffix('\n')
-    # remove file in path
+    
+    # remove file in from local storage
     os.remove(path)
+    
+    # Sending download link to user
     parts = link.split('/')
     parts.insert(3, 'get')
     download_link = '/'.join(parts)
     await message.reply_text(f"File's Download Page:\n\n{link}"
                              f'\n\nDirect Download Link:\n\n{download_link}'
-                             f'\n\nDownload Link will be valid for 2 weeks...')
+                             f'\n\nDownload Link will be valid for 2 weeks...', quote=True)
